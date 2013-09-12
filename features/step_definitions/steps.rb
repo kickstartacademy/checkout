a10e13154deb85bf91af37bf85380c7d904b4982
Matt Wynne <matt@mattwynne.net>
2013-09-12 14:13:10 +0200

    Convert tabs to spaces

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,67 +1,67 @@
 class Product
-	attr_accessor :price,:id
-	def initialize(price, id)
-		@price = price
-		@id = id
-	end
+  attr_accessor :price,:id
+  def initialize(price, id)
+    @price = price
+    @id = id
+  end
 end
 
 class Checkout
-	attr_accessor :total
-	def initialize
-		@total = 0
-	end
+  attr_accessor :total
+  def initialize
+    @total = 0
+  end
 
-	def scan(product, quantity)
-		@total += product.price * quantity
-	end
+  def scan(product, quantity)
+    @total += product.price * quantity
+  end
 end
 
 class Inventory
   def initialize
-  	@products = {}
+    @products = {}
   end
 
   def add(product)
-  	@products[product.id] = product
+    @products[product.id] = product
   end
 
   def find(id)
-  	@products.fetch(id)
+    @products.fetch(id)
   end
 end
 
 module CheckoutTestingDSL
-	def add_product(product)
-		inventory.add product
-	end
+  def add_product(product)
+    inventory.add product
+  end
 
-	def scan(product_id, number)
-		product = inventory.find(product_id)
-		checkout.scan(product, number)
-	end
+  def scan(product_id, number)
+    product = inventory.find(product_id)
+    checkout.scan(product, number)
+  end
 
-	def total
-		checkout.total
-	end
+  def total
+    checkout.total
+  end
 
-	def checkout
-		@checkout ||= Checkout.new
-	end
+  def checkout
+    @checkout ||= Checkout.new
+  end
 
-	def inventory
-		@inventory ||= Inventory.new
-	end
+  def inventory
+    @inventory ||= Inventory.new
+  end
 end
 
 World(CheckoutTestingDSL)
 
 Given(/^the price of a (\w+) is (\d+) EUR$/) do |product, price|
-	add_product Product.new(price, product)
+  add_product Product.new(price, product)
 end
 
 When(/^I scan (\d+) (\w+?)e?s?$/) do |number, product_id|
-	scan product_id, number
+  scan product_id, number
 end
 
 Then(/^the total should be (\d+) EUR$/) do |expected_total|
